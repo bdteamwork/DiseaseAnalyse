93574eed66a34771be87ba61b3b516cc9bd70f66
wby <wangbingyang@yongche.com>
2019-05-10 10:34:20 +0800

    补充文件

---
+++ src/main/java/DeseaseDepartmentSpark/SparkStatistics.java
@@ -0,0 +1,56 @@
+package DeseaseDepartmentSpark;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+
+import scala.Tuple2;
+
+public class SparkStatistics {
+	public static void main(String[] args) {
+		// 初始化核心操作对象
+		SparkConf conf = new SparkConf();
+		conf = conf.setAppName("deseasestatisticcs");
+		conf = conf.setMaster("local");
+		Map<String,Set> map = new HashMap<String,Set>();
+		
+		JavaSparkContext ctx = new JavaSparkContext(conf);
+		JavaRDD<String> lines = ctx.textFile("hdfs://192.168.16.130:9000/desease/in");
+
+		
+		JavaPairRDD<String, String> rddPair = lines.mapToPair(new PairFunction<String, String, String>() {
+			@Override
+			public Tuple2<String, String> call(String str) throws Exception {
+            	String[] res = str.split("\\|");
+            	if(res.length !=9){
+    				return new Tuple2<String, String>("1", "0");
+    			}
+				return new Tuple2<String, String>(res[0], res[1]);
+			}
+		});
+		rddPair = rddPair.reduceByKey(new Function2<String, String, String>(){
+			@Override
+			public String call(String arg0, String arg1) throws Exception {
+
+				if(arg0.contains(arg1)){
+					return arg0;
+				}else{
+					return arg0 + "/" + arg1;
+				}
+				
+			}
+		});
+//		
+		rddPair.saveAsTextFile("hdfs://192.168.16.130:9000/desease/statistic");
+		
+		
+
+		}
+}
---
+++ src/python词云图(王冰洋)/DeseaseDescription1.py
@@ -0,0 +1,76 @@
+# - * - coding: utf - 8 -*-
+
+from os import path
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+import jieba
+from wordcloud import WordCloud, ImageColorGenerator
+
+class generateCiYunDeseaseDescription():
+    # 获取当前文件路径
+    d = path.dirname(__file__)
+
+    stopwords = {}
+    isCN = 1 #默认启用中文分词
+    back_coloring_path = "111.png" # 设置背景图片路径
+    text_path = 'basicdata.txt' #设置要分析的文本路径
+    font_path = '经典综艺体简.ttf' # 为matplotlib设置中文字体路径没
+    stopwords_path = 'stopwords.txt' # 停用词词表
+    imgname1 = "DeseaseDescription.png" # 保存的图片名字1(只按照背景图片形状)
+
+    back_coloring = imread(path.join(d, back_coloring_path))# 设置背景图片
+
+    # 设置词云属性
+    wc = WordCloud(font_path=font_path,  # 设置字体
+                   background_color="white",  # 背景颜色
+                   max_words=1000,  # 词云显示的最大词数
+                   mask=back_coloring,  # 设置背景图片
+                   max_font_size=150,  # 字体最大值
+                   random_state=42,
+                   width=1000, height=860, margin=2,# 设置图片默认的大小,但是如果使用背景图片的话,那么保存的图片大小将会按照其大小保存,margin为词语边缘距离
+                   )
+
+    def jiebaclearText(text,stopwords_path,d):
+        mywordlist = []
+        seg_list = jieba.cut(text, cut_all=False)
+        liststr="/ ".join(seg_list)
+        f_stop = open(path.join(d,stopwords_path))
+        try:
+            f_stop_text = f_stop.read( )
+        finally:
+            f_stop.close( )
+
+        f_stop_seg_list=f_stop_text.split('\n')
+        for myword in liststr.split('/'):
+            if not(myword.strip() in f_stop_seg_list) and len(myword.strip())>1:
+                mywordlist.append(myword)
+        return ''.join(mywordlist)
+
+    if __name__ == '__main__':
+
+        # 打开文件，每行读取并存入text中做分词
+        file = open(path.join(d, text_path))
+        text = ""
+        while file.readline():
+            stringArray = file.readline().split('|')
+            text = text+stringArray[5]
+        file.close()
+
+        if isCN:
+            text = jiebaclearText(text,stopwords_path,d)
+
+
+        # 生成词云
+        wc.generate(text)
+        # 从背景图片生成颜色值
+        image_colors = ImageColorGenerator(back_coloring)
+
+        plt.figure()
+        # 以下代码显示图片
+        plt.imshow(wc)
+        plt.axis("off")
+        plt.show()
+        # 绘制词云
+
+        # 保存图片
+        wc.to_file(path.join(d, imgname1))
